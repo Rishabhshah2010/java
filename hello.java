bee0e4ab77cdbdedc9fa149635f8f58f3b1f73c4
Rishabhshah2010 <98174937+Rishabhshah2010@users.noreply.github.com>
2023-10-07 17:00:48 -0400

    practice

---
+++ hello.java
@@ -0,0 +1,11 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String args[]){
+        int num1 = 51, num2 = 96;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
---
+++ sales.java
@@ -0,0 +1,8 @@
+public class sales {
+    public static void main(String args[])
+    {
+        int cp = 500, sp = 660;
+        int profit = sp - cp;
+        System.out.println("The profit is Rs. " + profit);
+    }
+}
